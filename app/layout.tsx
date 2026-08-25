29d5c30e3ae64acf9d332586f0a693bb7eb26119
Nguyen Phuong Truong <nptruongart@gmail.com>
2026-08-25 21:23:15 +0700

    Them PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+export const metadata = {
+  title: 'Badminton Club',
+  description: 'App quản lý hội cầu lông',
+  manifest: '/manifest.json', // Dòng này gọi app PWA
+  themeColor: '#1890ff',
+}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
